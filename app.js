f31d6b32873222f6d075c6d198a7448ab693b8f6
MuhammadRaihanAlghifari <alghifari2096@gmail.com>
2024-05-18 16:51:51 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,23 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//  inisiasi router
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var AdminRouter = require('./routes/cth')
+var TesRouter = require('./routes/tes')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// deklarasi path 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', AdminRouter);
+app.use('/Coba', TesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("layouts/login1", { title: "Login" });
+});
+
+router.get("/password", function (req, res, next) {
+  res.render("layouts/password", { title: "Login" });
 });
 
 module.exports = router;
