1df9a4d37f09b86ddba1be6a39a7a86049003cb5
oktavianiandrianti26 <144988430+oktavianiandrianti26@users.noreply.github.com>
2024-06-10 22:17:05 +0700

    databuku_front end

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 //  inisiasi router
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+
 
 var app = express();
 
@@ -22,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // deklarasi path 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/databuku', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
+const middleware = require("../middleware/verifytoken")
 const controller = require('../controller/auth.controller')
+const profileLihat = require('../controller/lihatProfile')
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -8,11 +11,37 @@ router.get("/", function (req, res, next) {
 });
 
 
-router.post('/login', controller.checklogin);
+router.get('/login1', controller.form);
+router.post('/showProfile', controller.checklogin);
+router.post('/dashboard', controller.changePassword);
+router.post('/databuku', controller.changePassword);
+
+// router.post('/dashboard', async (req, res) => {
+//   try {
+//     await changePassword(req, res);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Terjadi kesalahan server" });
+//   }
+// });
+
+router.get("/dashboard", function (req, res, next) {
+  res.render("layouts/dashboard", { title: "Login" });
+});
+
+router.get("/editprofile", function (req, res, next) {
+  res.render("layouts/editprofile", { title: "Login" });
+});
+
+router.get("/showProfile", middleware.verifyToken, profileLihat.lihatProfil);
 
 
 router.get("/password", function (req, res, next) {
   res.render("layouts/password", { title: "Login" });
+});
+
+router.get("/databuku", function (req, res, next) {
+  res.render("layouts/databuku", { title: "Login" });
 });
 
 module.exports = router;
